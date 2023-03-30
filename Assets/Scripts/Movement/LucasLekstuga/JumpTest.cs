2fbde82dd3fa5689a81fbe5cb634acde5c52e453
LucasKarjel <lucas.karjel@gmail.com>
2023-03-30 16:23:10 +0200

    Movement

---
+++ Assets/Scripts/Movement/LucasLekstuga/JumpTest.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class JumpTest : MonoBehaviour
+{
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float jumpCooldown;
+    [SerializeField] private float glideForce;
+    [SerializeField] private bool readyToJump;
+    [SerializeField] private bool isDoubleJumping;
+
+    public bool canDoubleJump;
+    public float doubleJumpMultiplier;
+    public float coyoteTime;
+    public float coyoteTimeCounter;
+
+
+    [SerializeField]
+    public bool isGrounded;
+    private Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGrounded)
+        {
+            coyoteTimeCounter = coyoteTime;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+        if (isDoubleJumping)
+            Glide();
+    }
+
+    public void ButtonInput(InputAction.CallbackContext input)
+    {
+
+        if (input.started)
+        {
+            if (isGrounded && coyoteTimeCounter > 0 && readyToJump)
+            {
+                coyoteTimeCounter = 0;
+                Jump();
+
+                isGrounded = false;
+                readyToJump = false;
+
+                //Invoke(nameof(ResetJump), jumpCooldown);
+            }
+        }
+
+
+
+        if (input.canceled && !isGrounded && !isDoubleJumping)
+        {
+            canDoubleJump = true;
+        }
+
+        if (input.started && !isGrounded && canDoubleJump)
+        {
+            DoubleJump();
+        }
+
+        if (input.canceled && !isGrounded && isDoubleJumping)
+            isDoubleJumping = false;
+
+    }
+
+    private void Jump()
+    {
+        //rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            readyToJump = true;
+            canDoubleJump = false;
+            isDoubleJumping = false;
+        }
+    }
+
+    public void DoubleJump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        rb.AddForce(transform.up * (jumpForce * doubleJumpMultiplier), ForceMode.Impulse);
+        canDoubleJump = false;
+        isDoubleJumping = true;
+    }
+
+    public void Glide()
+    {
+        rb.AddForce(transform.up * glideForce, ForceMode.Force);
+    }
+
+
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
