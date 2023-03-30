cab361f30ae73e61b08c842569830ed4d4ce3c0c
JovinFast <112895166+JovinFast@users.noreply.github.com>
2023-03-30 08:56:42 +0200

    Checkpoint and ladder

---
+++ Assets/Scripts/Ladder/LadderScript2.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderScript2 : MonoBehaviour
+{
+    bool inside = false;
+    public Transform player;
+    public float speedUpDown = 3.2f;
+    public ForceMovement movementScript; //change depending on the type of movement script
+    Rigidbody playerRigidbody;
+    Jump jumpScript;
+    GameObject teleportPoint;
+    MaterialCheck materialCheckScript;
+    bool RunUpdate = false;
+    void Start()
+    {
+        materialCheckScript = GetComponent<MaterialCheck>();
+        jumpScript = GetComponent<Jump>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        movementScript = GetComponent<ForceMovement>();
+        inside = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ladder")
+        {
+            RunUpdate = true;
+            playerRigidbody.velocity = Vector3.zero;
+            movementScript.enabled = false;
+            inside = !inside;
+            //playerRigidbody.isKinematic = true;
+            playerRigidbody.useGravity = false;
+            jumpScript.enabled = false;
+        }
+        if (other.gameObject.tag == "GoDownLadder")
+        {        
+            if (inside == false)
+            {
+               teleportPoint = other.gameObject.GetComponent<GoDownTrigger>().teleportPoint;
+               transform.position = teleportPoint.transform.position;
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        
+        if (other.gameObject.tag == "Ladder")
+        {
+            RunUpdate = false;
+            playerRigidbody.useGravity = true;
+            //playerRigidbody.isKinematic = false;
+            movementScript.enabled = true;
+            inside = !inside;
+            jumpScript.enabled = true;
+        }
+    }
+    void Update()
+    {
+        if (RunUpdate == false) return;
+        if (inside == true && Input.GetKey("w"))
+        {
+            player.transform.position += Vector3.up / speedUpDown;
+        }
+        if (inside == true && Input.GetKey("s"))
+        {
+            player.transform.position += Vector3.down / speedUpDown;
+        }
+        if (inside == true && Input.GetKey(KeyCode.Space))
+        {
+            playerRigidbody.AddForce(-transform.forward * 100000f);
+        }
+
+
+
+        //Do raycast from player and check if grounded
+    }
+}
