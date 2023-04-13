0325b41bfaf12ad24da458e98824dd57a643df0b
eengdahl <engdahl.emil@gmail.com>
2023-04-13 14:59:30 +0200

    Camera CatAMole

---
+++ Assets/CameraFollowPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    GameObject thisCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisCam = GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
