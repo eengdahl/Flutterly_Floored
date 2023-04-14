94b4b45dae77da32333294af96e66bf5a33dfac1
JovinFast <112895166+JovinFast@users.noreply.github.com>
2023-04-14 15:27:48 +0200

    Fixed so we can build the game

--- Assets/Scripts/Cat-A-Mole/Paws.cs
+++ Assets/Scripts/Cat-A-Mole/Paws.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.PlasticSCM.Editor.WebApi;
+//using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 
 public class Paws : MonoBehaviour
--- Assets/Scripts/Testing/Water.cs
+++ Assets/Scripts/Testing/Water.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Serialization;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
+//using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class Water : MonoBehaviour
 {
