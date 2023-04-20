6bcd9dee376f39916aca5f9afcb625aac742da31
Heuhie <106738317+Heuhie@users.noreply.github.com>
2023-04-20 11:59:24 +0200

    Birds of Doom sound

---
+++ Assets/PreFab/Sink/EvilBirds/BirdOfDoom.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdOfDoom : MonoBehaviour
+{
+    public AudioSource doom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        doom.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
