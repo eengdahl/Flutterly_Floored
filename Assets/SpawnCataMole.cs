dc26dc4ba3650ea678b942830ddbdec7f3c1a5f7
JovinFast <112895166+JovinFast@users.noreply.github.com>
2023-04-20 14:24:40 +0200

    Spawn push

---
+++ Assets/SpawnCataMole.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCataMole : MonoBehaviour
+{
+    [Header("KillCatStuff")]
+    [SerializeField] GameObject cat;
+    [SerializeField] GameObject hitSpots;
+    [SerializeField] GameObject legs;
+    bool hasSpawned;
+    private void Start()
+    {
+        hasSpawned = false;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (hasSpawned) return;
+        if (other.CompareTag("Player"))
+        {
+            SpawnCat();
+            hasSpawned = true;
+        }
+    }
+
+    void SpawnCat()
+    {
+        legs.SetActive(true);
+        hitSpots.SetActive(true);
+        cat.SetActive(true);
+    }
+}
