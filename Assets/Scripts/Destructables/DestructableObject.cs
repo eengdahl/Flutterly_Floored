f5d320f613f41ca8fdf25739095c3d0453352302
Heuhie <106738317+Heuhie@users.noreply.github.com>
2023-04-25 11:44:16 +0200

    Destructables

--- Assets/Scripts/Destructables/DestructableObject.cs
+++ Assets/Scripts/Destructables/DestructableObject.cs
@@ -5,17 +5,48 @@ using UnityEngine;
 public class DestructableObject : MonoBehaviour
 {
     public GameObject destructionPrefab;
-    public GameObject destructableObject;
     public bool canTakeFallDamage;
     public bool canBeShot;
- 
+    public float breakThresholdHeight;
+
+    private GameObject destructableObject;
+    private Rigidbody rb;
+    private float fallheight;
+    private bool isFalling;
+    private float fallStartPosition;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if(rb.velocity.y < -0.1f && !isFalling)
+        {
+            fallStartPosition = transform.position.y;
+            isFalling = true;
+        }
+        if (isFalling)
+        {
+            CalculateFallheight();
+            Debug.Log(fallheight);
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (canTakeFallDamage && collision.gameObject.CompareTag("Ground"))
+        
+        if (canTakeFallDamage && collision.gameObject.CompareTag("Ground") && isFalling && fallheight > breakThresholdHeight)
         {
             Instantiate(destructionPrefab, transform.position, transform.rotation);
+            isFalling = false;
             Destroy(this.gameObject);
+        }
+        else if(collision.gameObject.CompareTag("Ground"))
+        {
+            isFalling = false;
         }
 
         if(canBeShot && collision.gameObject.CompareTag("Bullet"))
@@ -24,5 +55,12 @@ public class DestructableObject : MonoBehaviour
             destructableObject.GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity;
             Destroy(this.gameObject);
         }
+
+        
+    }
+
+    private void CalculateFallheight()
+    {
+        fallheight = fallStartPosition - transform.position.y;
     }
 }
