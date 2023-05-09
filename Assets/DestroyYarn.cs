febf73d76b872e9633405ff082f18606c976c3f2
JovinFast <112895166+JovinFast@users.noreply.github.com>
2023-05-09 09:58:53 +0200

    Yarn destroy

---
+++ Assets/DestroyYarn.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyYarn : MonoBehaviour
+{
+
+    [SerializeField] float destroyScale = 0.1914912f;
+    [SerializeField] GameObject seedPrefab;
+
+    private void Update()
+    {
+        if (transform.localScale.x <= destroyScale)
+        {
+            spawnSeed();
+        }
+    }
+
+    public void spawnSeed()
+    {
+        Instantiate(seedPrefab, transform.position,Quaternion.Euler(new Vector3 (0,0.11f,0)));
+        this.gameObject.SetActive(false);
+    }
+}
