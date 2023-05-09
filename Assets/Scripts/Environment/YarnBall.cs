f0779df63db8870026cb4683abc83093f99cec49
LucasKarjel <lucas.karjel@gmail.com>
2023-05-09 15:59:02 +0200

    Update YarnBall.cs

--- Assets/Scripts/Environment/YarnBall.cs
+++ Assets/Scripts/Environment/YarnBall.cs
@@ -8,7 +8,9 @@ public class YarnBall : MonoBehaviour
     private Rigidbody ballRB;
     private new ParticleSystem particleSystem;
     private Vector3 offset;
+    private float ballDirection;
     private float distanceTraveled;
+    private Vector3 psRotation;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +22,9 @@ public class YarnBall : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
+        //psRotation = new Vector3(particleSystem.startRotation3D.x, particleSystem.startRotation3D.y, particleSystem.startRotation3D.z);
+        ballDirection = ballRB.velocity.magnitude;
+        //psRotation = new Vector3(0, ballDirection, 0);
         distanceTraveled = ballRB.velocity.magnitude * Time.deltaTime;
         ballTransform.localScale -= new Vector3(distanceTraveled, distanceTraveled,distanceTraveled) * Time.deltaTime;
         offset = new Vector3(0, -ballTransform.localScale.y / 2 + 0.05f, 0);
