2dbd347a421be6a310c261bfdfb8f28570b2f367
eengdahl <engdahl.emil@gmail.com>
2023-05-09 17:04:39 +0200

    BirdSounds

---
+++ Assets/BirdSound.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdSound : MonoBehaviour
+{
+    public List<AudioClip> clipList;
+    AudioSource aS;
+    float randomTimer;
+
+    private void Start()
+    {
+        aS = GetComponent<AudioSource>();
+        randomTimer = 20;
+        Invoke(nameof(MakeRandomNoise), randomTimer);
+    }
+    private void MakeRandomNoise()
+    {
+        Debug.Log("ping");
+        aS.clip = clipList[Random.Range(0, clipList.Count - 1)];
+        aS.Play();
+        randomTimer = Random.Range(5, 20);
+        Invoke(nameof(MakeRandomNoise), randomTimer);
+    }
+}
