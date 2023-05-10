3a64d6fde3a1c8b7d0441cd2b0701a7c4b8a95e2
eengdahl <engdahl.emil@gmail.com>
2023-05-10 15:46:35 +0200

    scenandsound

--- Assets/CamCatAMole.cs
+++ Assets/CamCatAMole.cs
@@ -6,13 +6,20 @@ public class CamCatAMole : MonoBehaviour
 {
     public GameObject mainCam;
     public GameObject catACam;
+    AudioManager audioManager;
+    AudioSource aS;
 
-
+    private void Awake()
+    {
+        aS = GetComponent<AudioSource>();
+        audioManager = FindAnyObjectByType<AudioManager>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-
+            aS.Play();
+            audioManager.TurnOfMainMusic();
             catACam.SetActive(true);
             mainCam.SetActive(false);
         }
@@ -21,6 +28,8 @@ public class CamCatAMole : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
+            aS.Stop();
+            audioManager.ResumeMainMusic();
             catACam.SetActive(false);
             mainCam.SetActive(true);
         }
