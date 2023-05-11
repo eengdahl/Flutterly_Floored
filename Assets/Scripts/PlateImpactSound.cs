cdd56f316848bd4ec87a029812ace0b4acba9a58
JovinFast <112895166+JovinFast@users.noreply.github.com>
2023-05-11 21:35:38 +0200

    Fix meshes

---
+++ Assets/Scripts/PlateImpactSound.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateImpactSound : MonoBehaviour
+{
+
+    [SerializeField]AudioClip[] arrayOfImpactSounds;
+    AudioSource aS;
+    int soundIndex;
+    AudioClip aC;
+
+    private void Start()
+    {
+        aS = GetComponent<AudioSource>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag ("Player"))
+        {
+            soundIndex = Random.Range(0, arrayOfImpactSounds.Length);
+            aC = arrayOfImpactSounds[soundIndex];
+            aS.clip = aC;
+            aS.Play();
+        }
+    }
+
+}
