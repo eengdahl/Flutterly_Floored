ac7fee6e0329112f36bb415d4bb44758e705aa88
LucasKarjel <lucas.karjel@gmail.com>
2023-05-19 16:06:14 +0200

    collider fixes n stuff

--- Assets/Scripts/SwitchControls.cs
+++ Assets/Scripts/SwitchControls.cs
@@ -17,22 +17,28 @@ public class SwitchControls : MonoBehaviour
 
     public void SwitchToAir()
     {
-        playerInput.actions.FindActionMap("Floor").Disable();
-        playerInput.actions.FindActionMap("Climbing").Disable();
-        playerInput.actions.FindActionMap("WindZoneMovement").Enable();      
+        //playerInput.actions.FindActionMap("Floor").Disable();
+        //playerInput.actions.FindActionMap("Climbing").Disable();
+        //playerInput.actions.FindActionMap("WindZoneMovement").Enable();
+        playerInput.SwitchCurrentActionMap("WindZoneMovement");
+
     }
     public void SwitchToFloor()
     {
-        playerInput.actions.FindActionMap("Floor").Enable();
-        playerInput.actions.FindActionMap("Climbing").Disable();
-        playerInput.actions.FindActionMap("WindZoneMovement").Disable();
+        //playerInput.actions.FindActionMap("Floor").Enable();
+        //playerInput.actions.FindActionMap("Climbing").Disable();
+        //playerInput.actions.FindActionMap("WindZoneMovement").Disable();
+        playerInput.SwitchCurrentActionMap("Floor");
+
     }
 
     public void SwitchToClimbing()
     {
-        playerInput.actions.FindActionMap("Climbing").Enable();
-        playerInput.actions.FindActionMap("WindZoneMovement").Disable();
-        playerInput.actions.FindActionMap("Floor").Disable();
+        //playerInput.actions.FindActionMap("Climbing").Enable();
+        //playerInput.actions.FindActionMap("WindZoneMovement").Disable();
+        //playerInput.actions.FindActionMap("Floor").Disable();
+        playerInput.SwitchCurrentActionMap("Climbing");
+
     }
 
     public void SwitchToFlying()
