2a2b7120e211912c1d1288e045f6f0bb022a0f74
Heuhie <106738317+Heuhie@users.noreply.github.com>
2023-05-23 13:18:52 +0200

    Catamole Music

--- Assets/Scripts/KillCat.cs
+++ Assets/Scripts/KillCat.cs
@@ -7,6 +7,7 @@ public class KillCat : MonoBehaviour
     public GameObject puffPrefab;
     public Animator cataMoleAnimator;
     public GameObject catamoleDeathCube;
+    public GameObject catamoleCamSwap;
     AudioManager audiohandeler;
 
     private GameObject puffObject;
@@ -42,6 +43,7 @@ public class KillCat : MonoBehaviour
     {
         cataMoleAnimator.SetTrigger("DMKCat");
         yield return new WaitForSeconds(2);
+        catamoleCamSwap.SetActive(false);
         catamoleDeathCube.SetActive(false);
         audiohandeler.ResumeMainMusic();
         Destroy(puffObject);
