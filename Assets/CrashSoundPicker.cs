ae359ffcbd9578a0acf9e9a420e49739331b5c6e
JovinFast <112895166+JovinFast@users.noreply.github.com>
2023-05-23 22:34:23 +0200

    Breakables sound ready for testing

--- Assets/CrashSoundPicker.cs
+++ Assets/CrashSoundPicker.cs
@@ -7,13 +7,15 @@ public class CrashSoundPicker : MonoBehaviour
     AudioSource aS;
     [SerializeField] AudioClip[] sounds;
     int indexPicker;
-
+    float pitch;
     private void Awake()
     {
         aS = GetComponent<AudioSource>();
+        pitch = Random.Range(0.9f, 1.1f);
     }
     void Start()
     {
+        aS.pitch = pitch;
         indexPicker = Random.Range(0, sounds.Length);
         aS.clip = sounds[indexPicker];
         aS.Play();
