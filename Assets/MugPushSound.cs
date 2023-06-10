4fee705cf605923732062ba0957fc7ff9315cdcd
JovinFast <112895166+JovinFast@users.noreply.github.com>
2023-06-10 11:39:09 +0200

    Minnsi inte änns, gammla saker att pusha

--- Assets/MugPushSound.cs
+++ Assets/MugPushSound.cs
@@ -7,17 +7,25 @@ public class MugPushSound : MonoBehaviour
     AudioSource aS;
     [SerializeField]AudioClip[] pushSounds;
     int pushIndex;
+    bool canMakeSound;
     private void Start()
     {
+        canMakeSound = false;
         aS = GetComponent<AudioSource>();
+        Invoke(nameof(CanMakeSound),2);
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") ||other.CompareTag("Ground")&&canMakeSound)
         {
             pushIndex = Random.Range(0, pushSounds.Length);
             aS.clip = pushSounds[pushIndex]; 
             aS.Play();
         }
+    }
+
+    void CanMakeSound()
+    {
+        canMakeSound = true;
     }
 }
