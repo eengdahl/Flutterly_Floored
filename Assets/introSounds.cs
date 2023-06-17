14289ecc99f9dc102f19bad42bb1a53a1438bdc1
eengdahl <engdahl.emil@gmail.com>
2023-06-17 13:56:00 +0200

    Menu done!!

---
+++ Assets/introSounds.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class introSounds : MonoBehaviour
+{
+
+    AudioSource aS;
+    public AudioClip crash;
+    public AudioClip fall;
+    public AudioClip flapp;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aS = GetComponent<AudioSource>();
+    }
+
+    public void PlayFlapp()
+    {
+        aS.clip = flapp;
+        aS.Play();
+    }
+
+    public void PlayCrash()
+    {
+        aS.Stop();
+        aS.PlayOneShot(crash);
+    }
+
+    public void PlayFall()
+    {
+        aS.PlayOneShot(fall);
+    }
+}
