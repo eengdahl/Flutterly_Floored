be48c6955572c2719b74f97312e4f6dc70011a28
eengdahl <engdahl.emil@gmail.com>
2023-06-27 09:52:38 +0200

    CatFixed

--- Assets/Scripts/Cat-A-Mole/Defeated.cs
+++ Assets/Scripts/Cat-A-Mole/Defeated.cs
@@ -20,7 +20,7 @@ public class Defeated : MonoBehaviour
     {
         originalCamera.SetActive(true);
         windowBlock.SetActive(false);
-        camSwap.DisableZone();
+       // camSwap.DisableZone();
         gameObject.SetActive(false);
     }
 
